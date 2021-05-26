475f8733b0c593c88c8483e79e8607727c149a17
Chung Yunseong <chungys02@gmail.com>
2021-05-27 00:44:47 +0800

    login and signup routes

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,9 +1,63 @@
-function SignIn(props) {
-    return (
-        <div>
-            SignIn Page!
-        </div>
-    )
+import React, { Component } from "react";
+
+// function SignIn(props) {
+//     return (
+//         <div>
+//             SignIn Page!
+//         </div>
+//     )
+// }
+
+// export default SignIn;
+
+class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/SignIn", userData)
+      .then((res) => {
+        setAuthorizationHeader(res.data.token);
+        this.setState({
+          loading: false,
+          authenticated: true,
+        });
+        this.props.history.push("/List");
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  setAuthorizationHeader = (token) => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem("FBIdToken", FBIdToken);
+    axios.defaults.headers.common["Authorization"] = FBIdToken;
+  };
 }
 
 export default SignIn;
--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -1,9 +1,57 @@
-function SignUp(props) {
-    return (
-        <div>
-            SignUp Page!
-        </div>
-    )
-}
+import React, { Component } from "react";
+// function SignUp(props) {
+//     return (
+//         <div>
+//             SignUp Page!
+//         </div>
+//     )
+// }
 
-export default SignUp;
+// export default SignUp;
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      nickname: "",
+      errors: {},
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      nickname: this.state.handle,
+    };
+    axios
+      .post("/signup", newUserData)
+      .then((res) => {
+        setAuthorizationHeader(res.data.token);
+        this.setState({
+          loading: false,
+          errors: null,
+        });
+        this.props.history.push("/List");
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  setAuthorizationHeader = (token) => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem("FBIdToken", FBIdToken);
+    axios.defaults.headers.common["Authorization"] = FBIdToken;
+  };
+}
